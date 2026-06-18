8b43356f166a654d812aa522797a01738a866d76
Paulo <alvespaulo.834@gmail.com>
2026-06-18 13:45:44 -0300

    finalizando o modulo 6 e iniciando projeto usando express

---
+++ 6_EXPRESS/6_param_url/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 3000  // variavel ambiente
+
+const path = require('path')
+
+const basePath = path.join(__dirname, 'templates')
+
+
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id
+
+    //leitura da tabela users, resgatar um usuário do banco
+    console.log(`estamos buscando pelo usuário: ${id}`)
+    res.sendFile(`${basePath}/users.html`)
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`)
+})
+
+app.listen(port, () => {
+    console.log(`app rodando na porta ${port}`)
+})
