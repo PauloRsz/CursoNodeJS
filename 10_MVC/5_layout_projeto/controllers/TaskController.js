d51dd42e2f9a8dcf3ffceead33e47e1cb00d8cec
Paulo <alvespaulo.834@gmail.com>
2026-07-18 02:56:55 -0300

    Estrutura MVC

---
+++ 10_MVC/5_layout_projeto/controllers/TaskController.js
@@ -0,0 +1,11 @@
+const Task = require('../models/Task')
+
+module.exports = class TaskController {
+
+    static createTasks(req, res) {
+        res.render('tasks/create')
+    }
+    static showTasks(req, res) {
+        res.render('tasks/all')
+    }
+}
---
+++ 10_MVC/5_layout_projeto/routes/tasksRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+const TaskController = require('../controllers/TaskController')
+
+router.get('/add', TaskController.createTasks)
+router.get('/', TaskController.showTasks)
+
+
+module.exports = router
